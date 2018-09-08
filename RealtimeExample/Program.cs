a91ebab3ac6d52ad107d423fb4e68c9ce2352997
ksasao <k.sasao@gmail.com>
2018-09-09 01:31:43 +0900

    Post message example added.

--- RealtimeExample/Program.cs
+++ RealtimeExample/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RealtimeExample
@@ -16,15 +17,35 @@ namespace RealtimeExample
             // Legacy token is required for websocket client
             // https://api.slack.com/custom-integrations/legacy-tokens
             string accessTokenLegacy = "[Your Legacy Token]";
-
             _socketClient = new SlackSocketClient(accessTokenLegacy);
             _socketClient.OnMessageReceived += _socketClient_OnMessageReceived;
 
+
+            ManualResetEventSlim clientReady = new ManualResetEventSlim(false);
             _socketClient.Connect((connected) =>
             {
                 Console.WriteLine("Connected.");
+                clientReady.Set();
             });
-            Console.ReadKey();
+            clientReady.Wait(); // wait for connection
+
+            Console.WriteLine("Type some message. Enter to exit.");
+
+            while (true)
+            {
+                string text = Console.ReadLine();
+                if(text.Trim() == "")
+                {
+                    break;
+                }
+                string channelName = "bot";
+                string name = "Socket Client";
+
+                _socketClient.PostMessage(new Action<PostMessageResponse>(p =>
+                {
+                    Console.WriteLine(p.message.text);
+                }), channelName, text, name);
+            }
         }
 
         private static void _socketClient_OnMessageReceived(SlackAPI.WebSocketMessages.NewMessage obj)
@@ -37,6 +58,7 @@ namespace RealtimeExample
             string username = obj?.username ?? obj.user;
             string text = obj?.text ?? obj?.attachments[0].fallback ?? "";
             Console.WriteLine($"{date} {username}: {text}");
+
         }
     }
 }
